d649f090250d16b07f07584d1c6284b5f0c9bff7
EC2 Default User <ec2-user@ip-172-31-30-54.us-east-2.compute.internal>
2019-12-25 08:30:23 +0000

    新規登録画面

--- resources/lang/ja/messages.php
+++ resources/lang/ja/messages.php
@@ -1,10 +1,12 @@
 <?php
 return [
     'Login' =>'ログイン',
+    'Logout' => 'ログアウト',
     'E-Mail Address' => 'メールアドレス',
     'Password' => 'パスワード',
     'Remember Me' => 'ログイン情報を記憶する',
     'Register' => 'ユーザ登録',
     'Name' => '名前',
-    'Confirm Password' => 'パスワード確認',
+    'Confirm Password' => 'パスワード(確認)',
+    'Forgot Your Password' => 'パスワードを忘れましたか？',
     ];
--- resources/views/admin/new/create.blade.php
+++ resources/views/admin/new/create.blade.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html>
-    <head>
-     <meta charset="utf-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viesport" content="width=device-width, initial-scale=1">
-     <title>新規クライアント</title>
-    </head>
-    <body>
-        <h1>新規クライアント作成</h1>
-    </body>
-</html>
+@extends('layouts.admin')
+
+
+@section('title', '新規クライアントの作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>新規クライアント作成</h2>
+                
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
